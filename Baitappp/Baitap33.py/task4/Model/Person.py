1106fb1d0243881e0e489aec7e6d5e16c2bea5a1
hoangkimquyphi <104552063+hoangkimquyphi@users.noreply.github.com>
2023-05-31 10:58:46 +0700

    Delete Baitappp/Baitap33.py/task4 directory

--- Baitappp/Baitap33.py/task4/Model/Person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, id, code, firstName, lastName, birthOfDate):
-        self.id = id
-        self.code = code
-        self.firstName = firstName
-        self.lastName = lastName
-        self.birthOfDate = birthOfDate
-        
--- Baitappp/Baitap33.py/task4/Model/Student.py
+++
@@ -1,9 +0,0 @@
-from Model.Person import Person
-
-class Student(Person):
-    def __init__(self, id, code, firstName, lastName, birthOfDate, math, physics, chemistry):
-        super().__init__(id, code, firstName, lastName, birthOfDate)
-        self.math = math
-        self.physics = physics
-        self.chemistry = chemistry
-        
